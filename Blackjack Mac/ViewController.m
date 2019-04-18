fed0125a98448c1c8dd2cf796c8903aade9515d5
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2019-04-18 08:26:46 +0200

    Create project

---
+++ Blackjack Mac/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  Blackjack Mac
+//
+//  Created by Alessandro Vinciguerra on 2019-04-18.
+//      <alesvinciguerra@gmail.com>
+//  Copyright (C) 2019 Arc676/Alessandro Vinciguerra
+
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation (version 3)
+
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//  See README and LICENSE for more details
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+
+	// Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+	[super setRepresentedObject:representedObject];
+
+	// Update the view, if already loaded.
+}
+
+
+@end
---
+++ README.md
@@ -0,0 +1,7 @@
+# Blackjack Mac
+
+A Cocoa frontend to the [Rust Blackjack library](https://github.com/Arc676/Blackjack)
+
+## Licensing
+
+Project available under GPLv3. See `LICENSE` for full license text.
